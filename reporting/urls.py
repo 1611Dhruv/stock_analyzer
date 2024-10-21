37380b0bc86e7d2d0563808ac3d023e860c9d352
Dhruv Desai <ddesai7@wisc.edu>
2024-10-21 00:14:05 -0500

    Updated reporting bug

--- reporting/urls.py
+++ reporting/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from financial_data.views import stock_analyzer_view, view_data
+from reporting.views import reporting_view
 
 urlpatterns = [
-    path("", stock_analyzer_view, name="stock_analyzer_view"),
-    path("data", view_data, name="view_data"),
+    path("", reporting_view, name="reporting_view"),
 ]
