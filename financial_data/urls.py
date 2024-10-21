2fa1a86dcec30acb99adadb46d5a885b58f7ecaa
Dhruv Desai <ddesai7@wisc.edu>
2024-10-21 00:41:50 -0500

    Updated the URS so they actually work

--- financial_data/urls.py
+++ financial_data/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from financial_data.views import stock_analyzer_view, view_data
+
+from financial_data.views import add_symbol_view, list_data_view, refresh_symbols_view
 
 urlpatterns = [
-    path("", stock_analyzer_view, name="stock_analyzer_view"),
-    path("data", view_data, name="view_data"),
+    path("add/", add_symbol_view),
+    path("refresh/", refresh_symbols_view),
+    path("data/", list_data_view),
 ]
--- financial_data/views.py
+++ financial_data/views.py
@@ -1,23 +1,21 @@
-import json
 import logging
 from typing import Dict
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.http.response import HttpResponseBadRequest
-from django.shortcuts import render
+
 from financial_data.tasks import add_symbol, get_data, get_symbols, periodic_update
 
 logger = logging.getLogger("financial_data")
 logger.setLevel(logging.CRITICAL)
 
 
-def stock_analyzer_view(request: HttpRequest) -> HttpResponse:
+def add_symbol_view(request: HttpRequest) -> HttpResponse:
     """
-    The default stock_analyzer_view
+    The post request endpoint which adds a symbol to the database
     """
-    logger.info(request)
     if request.method == "POST":
-        if "add_symbol" in request.POST:
+        if "symbol" in request.POST:
             # Try getting the symbol object from the POST request
             symbol = request.POST.get("symbol")
             if symbol is None:
@@ -30,25 +28,29 @@ def stock_analyzer_view(request: HttpRequest) -> HttpResponse:
                 logger.error(e)
                 return HttpResponseBadRequest(e)
 
-        elif "refresh_existing" in request.POST:
-            periodic_update()
-
-    return render(request, "stock_analyzer.html")
+    return HttpResponseBadRequest("Invalid request")
 
 
-def view_data(request: HttpRequest) -> HttpResponse:
+def refresh_symbols_view(request: HttpRequest) -> HttpResponse:
     """
-    Endpoint to view all the data
+    The post request endpoint which adds a symbol to the database
+    """
+    if request.method == "POST":
+        periodic_update()
+    return HttpResponseBadRequest("Invalid request")
+
+
+def list_data_view(request: HttpRequest) -> HttpResponse:
+    """
+    Endpoint to view all the existing stock data in the database
     """
     symbol = request.GET.get("symbol", None)
     json_resp: Dict = {"status": "OK"}
 
     if symbol is None:
         json_resp["data"] = get_symbols()
-        json_data = json.dumps(json_resp)
-        return HttpResponse(json_data, content_type="application/json")
+        return JsonResponse(json_resp)
 
     json_resp["data"] = get_data(symbol)
     json_resp["symbol"] = symbol
-    json_data = json.dumps(json_resp)
-    return HttpResponse(json_data, content_type="application/json")
+    return JsonResponse(json_resp)
